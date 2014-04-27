1603a8d9e43a6be6b3bc4d2a5902c63e0336012f
stefanik.mich@gmail.com <stefanik.mich@gmail.com@3ad3e2aa-26c5-5c50-e237-cb28fdfea78e>
2014-04-27 11:13:16 +0000

    test skeleton added

---
+++ test/ParserTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Michal Štefánik 422237 <https://is.muni.cz/auth/osoba/422237>
+ */
+public class ParserTest {
+    
+    public ParserTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
