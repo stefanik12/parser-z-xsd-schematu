84a1b18a9a4c1eec24f69e77d85bb6f7f3d70b30
sven.relovsky@gmail.com <sven.relovsky@gmail.com@3ad3e2aa-26c5-5c50-e237-cb28fdfea78e>
2014-06-10 18:00:44 +0000

    Interface pre zakladne metody kt. budem implementovat(moze sa menit)

---
+++ src/parser/BinderInterface.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package parser;
+
+/**
+ *
+ * @author Sven
+ */
+public interface BinderInterface {
+    
+    
+    /**
+     * 
+     * for all number types should return double and for all other types String
+     * 
+     * @param converted is type in XML format(xsd?) 
+     * @return datatype in String
+     */
+    String  convertType(String converted);
+    
+    /**
+     * opens new Buffered reader  and creates BinderFile class
+     * 
+     */
+    void run();
+    
+    /** 
+     * creating java classes from  complex types
+     */
+    void complexToClass();
+    
+    /**
+     * creating java variable from simple methods(este to mozno bude inak)
+     */
+    void simpleToVar();
+}
